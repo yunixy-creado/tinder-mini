75cfd775d008c553b111cab52ba4a09d869d36be
naopandao <nao_yoshida@yunixy.co.jp>
2022-07-21 21:39:01 +0900

    YX-113 ボタンをクリックしたらスワイプする機能を実装

--- app/javascript/src/swipe.js
+++ app/javascript/src/swipe.js
@@ -63,5 +63,31 @@ if(location.pathname == "/users") {
         }
       });
     });
+    function createButtonListener(reaction) {
+      let cards = document.querySelectorAll('.swipe--card:not(.removed)');
+
+  if (!cards.length) return false;
+
+  let moveOutWidth = document.body.clientWidth * 2;
+
+  let card = cards[0];
+  card.classList.add('removed');
+
+  if (reaction == "like") {
+    card.style.transform = 'translate(' + moveOutWidth + 'px, -100px) rotate(-30deg)';
+  } else {
+    card.style.transform = 'translate(-' + moveOutWidth + 'px, -100px) rotate(30deg)';
+  }
+
+  initCards();
+    }
+
+    $('#like').on('click', function() {
+      createButtonListener("like");
+    })
+
+    $('#dislike').on('click', function() {
+      createButtonListener("dislike");
+    })
   });
 }
