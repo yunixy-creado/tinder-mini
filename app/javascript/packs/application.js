65ffabafc2b32e61425cd4d8bb0e028094c6903d
naopandao <nao_yoshida@yunixy.co.jp>
2022-07-21 21:07:36 +0900

    YX-113 スワイプ機能の実装

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -5,6 +5,7 @@ import '@fortawesome/fontawesome-free/js/all';
 
 
 
+
 // This file is automatically compiled by Webpack, along with any other files
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
@@ -15,6 +16,7 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 require("src/profile_image_upload");
+require("src/swipe")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
