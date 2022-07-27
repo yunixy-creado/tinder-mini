f91d50d24723707c24dafa721074a6d6172a63b2
naopandao <nao_yoshida@yunixy.co.jp>
2022-07-27 21:45:49 +0900

    YX-116 ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ Rails.application.routes.draw do
   resources :users, only: [:show, :index]
   resources :reactions, only: [:create]
   resources :matching, only: [:index]
+  resources :chat_rooms, only: [:create, :show]
 
 end
 
